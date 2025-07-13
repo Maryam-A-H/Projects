5af14b5203e59d19ecff3030e5b5d3676f63f89c
Maryam Ahmed <maryam_ahmeddd@hotmail.com>
2025-07-13 14:51:54 -0600

    Update Game.py

--- Game.py
+++ Game.py
@@ -36,7 +36,7 @@ st.markdown(
 
 st.markdown(
     """
-    <h6 style="color: black; font-style: italic; font-family: 'Georgia', serif; 
+    <h6 style="color: black; font-family: 'Georgia', serif; 
                text-align: center; margin-top: 5px;">
         Really dig into your dataset. Find out what's there, is it just text? What's the distribution of the dataset? <b>Most Important Step</b>
     </h6>
